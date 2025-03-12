1d7fbb6ad3f09cfd0e0ba4da22c134d944e8ccf3
amiclot <amiclot09@gmail.com>
2025-03-12 12:51:55 -0400

    Increase password hash field length to fix Heroku deployment

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ auth = HTTPBasicAuth()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
